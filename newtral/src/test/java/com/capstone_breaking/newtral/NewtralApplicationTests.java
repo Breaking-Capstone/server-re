fb1ab4a942369c13631d7d048c8ab7abd3e17930
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-04-08 22:02:06 +0900

    chore: yml 포트번호 변경

--- newtral/src/test/java/com/capstone_breaking/newtral/NewtralApplicationTests.java
+++ newtral/src/test/java/com/capstone_breaking/newtral/NewtralApplicationTests.java
@@ -3,11 +3,11 @@ package com.capstone_breaking.newtral;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class NewtralApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class NewtralApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
